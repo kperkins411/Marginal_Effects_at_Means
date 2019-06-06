f284ce548ea458d46acfe84f58a8b0138b56ddf2
Keith <kperkins411@gmail.com>
2019-06-05 23:50:00 -0400

    Initial commit

---
+++ mem.py
@@ -0,0 +1,105 @@
+import pandas as pd
+from numpy.random import randint
+import numpy as np
+
+class MEMs():
+    """
+    Marginal Effects At Means - used to get average value for all columns and then permute 1 column to see what model predicts
+
+    NOTE: For int64 columns the average of the column is the integer value closest to the average computed for the column
+    For instance if column average=4.5 and the column contains a mix of 1,3,5,7,9 ints.   The also will select 5
+    since thats the closest to 4.5
+
+    makes copy of passed in dataframe, creates a 1 row dataframe (df_avg)  that contains
+    the average for each column. For a particular column, generates vals, allows selection
+    of a range of values spanning col.min() to col.max()
+    Then allows permutation of 1 column at a time in df_avg, Using a single value in vals
+    Uses the row in df_avg (with the new val in column of interest) to predict outcome
+    for each value in vals.
+    """
+
+    def find_nearest(self,array, value):
+        array = np.asarray(array)
+        idx = (np.abs(array - value)).argmin()
+        return array[idx]
+
+    def __init__(self,df):
+        '''
+        takes in a dataframe, creates a copy with single row, averages each df column and puts it in copys column
+        :param df: dataframe, numeric
+        '''
+        self.df_orig = df
+        self.df_avg = pd.DataFrame(data=None,columns=df.columns, index=[0])  #create empty dataframe with same columns
+
+        #average df, put value into appropriate column of df_avg
+        #WHAT TO DO ABOUT CATEGORICAL WITH @ OR $ VALUES? WHAT IS THE MEAN?
+        for col in self.df_avg.columns:
+            if (self.df_orig[col].dtype == np.int64):
+                vals = df[col].unique()    #get unique values
+                mn = df[col].mean()        #get the mean
+                self.df_avg.at[0, col] = self.find_nearest(vals, mn)
+            elif (self.df_orig[col].dtype == np.float64):
+                self.df_avg.at[0,col] = df[col].mean()
+            else:
+                raise TypeError(f"All input columns must be an integer or a float, column {col} is a {str(self.df_orig[col].dtype)}")
+
+    def getMEM(self,model, col,number_steps=10.0 ):
+        """
+        :param model: random forest
+        :param col: which column to operate on, string (ex. "b") or index (ex. 0)
+        :param number_steps: how many iterations
+        :return: list of (col_val, prediction)
+        finds range of column in self.df_orig, create list with number_steps going from range.start to range.end
+        runs predictions on self.df_avg with those ranged values
+        """
+
+        vals = self._getRangeList(col, number_steps)
+        preds=[]
+
+        #if its an int get the string column name
+        if (type(col) is int):
+            col = self.df_orig.columns[col]
+
+        #run predictor for every value in vals over the average of the other columns
+        for val in vals:
+            df_avgtmp = self.df_avg.copy()
+            df_avgtmp.at[0, col]=val
+            preds.append((val, model.predict(df_avgtmp)))
+        return preds
+
+    def _getRangeList(self, col, number_samples):
+        """
+        gets number_samples starting at col.min() and ending at col.max()
+        if column is categorical (int64) will be less if there are not number_samples unique values
+        :param col: which column, string (ex. "b") or index (ex. 0)
+        :param number_samples: how many you want,
+        :return: list of values, uniformly distributed between start and finish
+        """
+        if (type(col) is int):
+            vals = self.df_orig.iloc[:,col]
+        else:
+            vals=self.df_orig[col]
+
+        unique_vals = np.sort(vals.unique())
+        tpe = unique_vals.dtype
+        luv = len(unique_vals)
+
+
+        # choose smaller of the 2
+        if (luv < number_samples):
+            number_samples = luv
+
+        res=[]
+        if ( tpe == 'int64'):
+            #its categorical
+            step_size= int(luv / (number_samples - 1))
+            res= unique_vals[0:luv:step_size]
+            if (len(res)<number_samples):
+                res = np.append(res,[-1])
+            elif (len(res)>number_samples):
+                res=res[:number_samples]
+            res[len(res)-1]=vals.max()
+        elif (tpe == 'float64'):
+            res = np.linspace(vals.min(), vals.max(), number_samples)
+
+        return res
---
+++ test_sumFeat.py
@@ -0,0 +1,175 @@
+from unittest import TestCase
+import pandas as pd
+from numpy.random import randint
+import numpy as np
+from mem import MEMs
+
+class mod():
+    def predict(self,row):
+        """
+
+        :param val: assumes single row dataframe
+        :return:
+        """
+        return row.sum(axis=1)[0]*2
+
+class TestgetMem(TestCase):
+    def setUp(self):
+        pass
+
+
+class TestMEMs(TestCase):
+    def setUp(self):
+        self.dfo_scrambled = pd.DataFrame([[9,10,11],[6,7,8],[3,4,5],[12,13,14],[0,1,2]], columns=list('abc'))
+        self.dfe_scrambled = pd.DataFrame([[9, 10, 11], [6, 7, 8], [12, 13, 14], [0, 1, 2]],columns=list('abc'))
+
+        self.df111 = pd.DataFrame([[0,0,1],[1,11,5],[2,22,19]], columns=list('abc'))
+        self.df1 = pd.DataFrame(np.arange(12).reshape(4, 3), columns=list('abc'))
+        self.df2 = pd.DataFrame(np.arange(15).reshape(5, 3), columns=list('abc'))
+        self.df11 = pd.DataFrame(np.arange(60).reshape(20, 3), columns=list('abc'))
+        self.df22 = pd.DataFrame(np.arange(63).reshape(21, 3), columns=list('abc'))
+        self.col='a'
+        self.col0=0#
+        self.df1f = pd.DataFrame(np.linspace(1.0, 10.0,12).reshape(4, 3), columns=list('abc'))
+        self.df2f = pd.DataFrame(np.linspace(1.0, 10.0,15).reshape(5, 3), columns=list('abc'))
+        self.df11f = pd.DataFrame(np.linspace(1.0, 10.0,60).reshape(20, 3), columns=list('abc'))
+        self.df22f = pd.DataFrame(np.linspace(1.0, 10.0,63).reshape(21, 3), columns=list('abc'))
+
+    def test_handle_even_int_column_mean_choice(self):
+        # small even
+        dfc = MEMs(self.df111)
+        a=dfc.df_avg.iloc[0, 0]
+        self.assertTrue(dfc.df_avg.iloc[0,0]==1)
+        self.assertTrue(dfc.df_avg.iloc[0, 1] == 11)
+        self.assertTrue(dfc.df_avg.iloc[0, 2] == 5)
+
+    def test_getMEM_small(self):
+        # small even
+        dfc = MEMs(self.df1)
+        m=mod()
+        preds = dfc.getMEM(m,'b')
+        self.assertTrue(preds== [(1, 18.0), (4, 24.0), (7, 30.0), (10, 36.0)])
+        self.assertRaises(KeyError, dfc._getRangeList, 'A', 10) #A does not exist
+        self.assertRaises(KeyError, dfc._getRangeList, 'd', 10) #d "
+        preds = dfc.getMEM(m, 1)
+        self.assertTrue(preds== [(1, 18.0), (4, 24.0), (7, 30.0), (10, 36.0)])
+
+        # small odd
+        dfc = MEMs(self.df2)
+        m = mod()
+        self.assertRaises(KeyError, dfc._getRangeList, 'A', 10) #A does not exist
+        self.assertRaises(KeyError, dfc._getRangeList, 'd', 10) #d "
+        preds = dfc.getMEM(m,'b')
+        self.assertTrue(preds== [(1, 30.0), (4, 36.0), (7, 42.0), (10, 48.0),(13,54.0)])
+
+        #scrambled
+        dfc = MEMs(self.dfo_scrambled)
+        m=mod()
+        preds = dfc.getMEM(m,'b')
+        self.assertTrue(preds== [(1, 30.0), (4, 36.0), (7, 42.0), (10, 48.0),(13,54.0)])
+
+        dfc = MEMs(self.dfe_scrambled)
+        m = mod()
+        preds = dfc.getMEM(m, 'b')
+        self.assertTrue(preds == [(1, 30.0), (7, 42.0), (10, 48.0), (13, 54.0)])
+
+
+    def test_getMEM_large(self):
+        dfc = MEMs(self.df1)
+        m = mod()
+        self.assertRaises(KeyError, dfc._getRangeList, 'A', 10)  # A does not exist
+        self.assertRaises(KeyError, dfc._getRangeList, 'd', 10)  # d "
+
+        preds = dfc.getMEM(m, 'b')
+        self.assertTrue(preds == [(1, 18.0), (4, 24.0), (7, 30.0), (10, 36.0)])
+        preds = dfc.getMEM(m, 1)
+        self.assertTrue(preds == [(1, 18.0), (4, 24.0), (7, 30.0), (10, 36.0)])
+        pass  # get first column
+
+    def test__getRangeList_ieven(self):
+        sf = MEMs(self.df1)
+        l= sf._getRangeList(0,10)
+        self.assertTrue( (l==[0,3,6,9]).all() )
+        l= sf._getRangeList(self.col,10)
+        self.assertTrue( (l==[0,3,6,9]).all() )
+
+    def test__getRangeList_iodd(self):
+        sf = MEMs(self.df2)
+        l= sf._getRangeList(0,10)
+        self.assertTrue( (l==[0,3,6,9,12]).all() )
+        l = sf._getRangeList(self.col, 10)
+        self.assertTrue((l == [0, 3, 6, 9, 12]).all())
+    def test__getRangeList_ieven_lrg(self):
+        sf = MEMs(self.df11)
+        l= sf._getRangeList(0,10)
+        self.assertTrue( (l==[0,6,12,18,24,30,36,42,48,57]).all() )
+        l= sf._getRangeList(self.col,10)
+        self.assertTrue( (l==[0,6,12,18,24,30,36,42,48,57]).all() )
+        l = sf._getRangeList(self.col, 3)
+        self.assertTrue((l == [0, 30, 57]).all())
+    def test__getRangeList_iodd_lrg(self):
+        sf = MEMs(self.df22)
+        l= sf._getRangeList(0,10)
+        self.assertTrue( (l==[0,6,12,18,24,30,36,42,48,60]).all() )
+        l = sf._getRangeList(self.col, 10)
+        self.assertTrue((l ==[0,6,12,18,24,30,36,42,48,60]).all())
+        l = sf._getRangeList(self.col, 3)
+        self.assertTrue((l ==[0,30,60]).all())
+
+    def check1(self, l, expectedvals):
+        self.assertTrue(len(l) == expectedvals)  # make sure there are 4
+        self.assertTrue(all(sorted(l) == l))  # make sure its sorted
+        self.assertTrue(all(l[i] <= l[i + 1] for i in range(len(l) - 1)))  # make sure they are different
+
+    def test__getRangeList_feven(self):
+        sf = MEMs(self.df1f)
+        l = sf._getRangeList(0, 10)
+        self.check1(l,4)
+        l = sf._getRangeList(self.col, 10)
+        self.check1(l,4)
+        l = sf._getRangeList(self.col, 3)
+        self.check1(l,3)
+        l = sf._getRangeList(self.col, 2)
+        self.check1(l, 2)
+
+    def test__getRangeListf_fodd(self):
+        sf = MEMs(self.df2f)
+        l = sf._getRangeList(0, 10)
+        self.check1(l, 5)
+        l = sf._getRangeList(self.col, 10)
+        self.check1(l, 5)
+        l = sf._getRangeList(self.col, 5)
+        self.check1(l, 5)
+        l = sf._getRangeList(self.col, 3)
+        self.check1(l, 3)
+        l = sf._getRangeList(self.col, 2)
+        self.check1(l, 2)
+
+    def test__getRangeList_feven_lrg(self):
+        sf = MEMs(self.df11f)
+        l = sf._getRangeList(0, 10)
+        self.check1(l, 10)
+        l = sf._getRangeList(self.col, 10)
+        self.check1(l, 10)
+        l = sf._getRangeList(self.col, 5)
+        self.check1(l, 5)
+        l = sf._getRangeList(self.col, 3)
+        self.check1(l, 3)
+        l = sf._getRangeList(self.col, 2)
+        self.check1(l, 2)
+
+
+    def test__getRangeList_fodd_lrg(self):
+        sf = MEMs(self.df22f)
+        l = sf._getRangeList(0, 10)
+        self.check1(l, 10)
+        l = sf._getRangeList(self.col, 10)
+        self.check1(l, 10)
+        l = sf._getRangeList(self.col, 5)
+        self.check1(l, 5)
+        l = sf._getRangeList(self.col, 3)
+        self.check1(l, 3)
+        l = sf._getRangeList(self.col, 2)
+        self.check1(l, 2)
+
+
